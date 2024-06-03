9158d21ac594a720c12a7587b26f65c7e9c17890
mustakshif <mustakshif@icloud.com>
2024-06-03 16:36:32 +0800

    introduce new api version

---
+++ src/api.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2023 frostime. All rights reserved.
+ * https://github.com/frostime/sy-plugin-template-vite
+ * 
+ * See API Document in [API.md](https://github.com/siyuan-note/siyuan/blob/master/API.md)
+ * API 文档见 [API_zh_CN.md](https://github.com/siyuan-note/siyuan/blob/master/API_zh_CN.md)
+ */
+
+import { fetchSyncPost, IWebSocketData } from "siyuan";
+
+
+async function request(url: string, data: any) {
+    let response: IWebSocketData = await fetchSyncPost(url, data);
+    let res = response.code === 0 ? response.data : null;
+    return res;
+}
+
+
+// **************************************** File ****************************************
+
+export async function getFile(path: string): Promise<any> {
+    let data = {
+        path: path
+    }
+    let url = '/api/file/getFile';
+    try {
+        let file = await fetchSyncPost(url, data);
+        return file;
+    } catch (error_msg) {
+        return null;
+    }
+}
+
+export async function putFile(path: string, isDir: boolean, file: any) {
+    let form = new FormData();
+    form.append('path', path);
+    form.append('isDir', isDir.toString());
+    // Copyright (c) 2023, terwer.
+    // https://github.com/terwer/siyuan-plugin-importer/blob/v1.4.1/src/api/kernel-api.ts
+    form.append('modTime', Math.floor(Date.now() / 1000).toString());
+    form.append('file', file);
+    let url = '/api/file/putFile';
+    return request(url, form);
+}
+
+export async function removeFile(path: string) {
+    let data = {
+        path: path
+    }
+    let url = '/api/file/removeFile';
+    return request(url, data);
+}
