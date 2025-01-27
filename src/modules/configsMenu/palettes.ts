f38b3caadb1dd7df46d921525ab98b15a86553a0
mustakshif <mustakshif@icloud.com>
2025-01-27 16:26:29 +0800

    feat(configsMenu): add Auriflow and Everbliss preset palettes

---
+++ src/modules/configsMenu/palettes.ts
@@ -0,0 +1,17 @@
+const prstDarkAuriflow = {
+    id: "Auriflow",
+    configs: {
+        'primary': '#F4CA57',
+        'chroma': '1',
+        'followSysAccentColor': false,
+    }
+}
+
+const prstLightEverbliss = {
+    id: "Everbliss",
+    configs: {
+        'primary': '#F4CA57',
+        'chroma': '1',
+        'followSysAccentColor': false,
+    }
+}
