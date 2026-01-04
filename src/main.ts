693f1da0d52e19d801871c1392585273864bc20f
mustakshif <mustakshif@icloud.com>
2026-01-04 22:56:30 +0800

    chore: remove empty code change entries

--- src/main.ts
+++ src/main.ts
@@ -52,20 +52,20 @@ async function unloadAsriJSModules(completeUnload = true) {
   await unloadAsriTheme(completeUnload);
 }
 
-window.destroyTheme = () => {
-  let completeUnload = true;
+// window.destroyTheme = () => {
+//   let completeUnload = true;
 
-  setTimeout(() => {
-    const themePairOnDestroy = {
-      "light": window.siyuan.config.appearance.themeLight,
-      "dark": window.siyuan.config.appearance.themeDark,
-    };
-    console.log("destroy", themePairOnDestroy);
-    if (themePairOnDestroy.light === "Asri" && themePairOnDestroy.dark === "Asri") completeUnload = false;
+//   setTimeout(() => {
+//     const themePairOnDestroy = {
+//       "light": window.siyuan.config.appearance.themeLight,
+//       "dark": window.siyuan.config.appearance.themeDark,
+//     };
+//     console.log("destroy", themePairOnDestroy);
+//     if (themePairOnDestroy.light === "Asri" && themePairOnDestroy.dark === "Asri") completeUnload = false;
 
-    console.log(`\x1b[31m\x1b[1m\x1b[47mTheme destroy mode: ${completeUnload ? "complete" : "incomplete"}\x1b[0m`);
-    unloadAsriJSModules(completeUnload);
-  }, 0);
-};
+//     console.log(`\x1b[31m\x1b[1m\x1b[47mTheme destroy mode: ${completeUnload ? "complete" : "incomplete"}\x1b[0m`);
+//     unloadAsriJSModules(completeUnload);
+//   }, 0);
+// };
 
-loadAsriJSModules();
+// loadAsriJSModules();
