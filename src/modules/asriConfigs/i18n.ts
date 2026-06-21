a424666c0012438580e69135a9b5e7306e160c40
mustakshif <mustakshif@icloud.com>
2026-06-21 17:45:04 +0800

    Fix i18n locale loading and bump version

--- src/modules/asriConfigs/i18n.ts
+++ src/modules/asriConfigs/i18n.ts
@@ -3,13 +3,15 @@ import { environment as env } from "../../util/rsc";
 export async function loadI18n() {
   let res: Response;
   try {
-    if (["zh_CN", "zh_CHT", "en_US", "ar_SA"].includes(env.lang)) {
-      res = await fetch(`/appearance/themes/Asri/i18n/${env.lang}.json`);
+    console.log("loading i18n for lang:", env.lang, typeof env.lang);
+    if (["zh-CN", "zh-CHT", "en-US", "ar-SA"].includes(env.lang)) {
+      res = await fetch(`/appearance/themes/Asri/i18n/${env.lang.replace("-", "_")}.json`);
     } else {
       res = await fetch("/appearance/themes/Asri/i18n/en_US.json");
     }
 
     const i18n = await res.json();
+    console.log("i18n:", i18n);
     return i18n;
   } catch (error) {
     console.error("failed to load i18n:", error);
